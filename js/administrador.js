0c11f5d1efd9377ae95e4e6f94247c1ef934ee78
Richar Irasabal Belles <irasabal.richy@gmail.com>
2022-04-07 19:36:31 -0400

    Se añadio la interfaz de administrador.html, administrador.js, style_administrador.css, img perfil2.jpg, vendedor.jpg(fondo de pantalla de los interfaces) y se agrego la carpeta js, para los archivos js

---
+++ js/administrador.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector('#menu-btn');
+        const menu = document.querySelector('#sidemenu');
+        btn.addEventListener('click', e =>{
+            menu.classList.toggle("menu-collapsed");
+            menu.classList.toggle("menu-expanded");
+            
+
+            document.querySelector('body').classList.toggle('body-expanded');
+        });
+
+
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+    target.addEventListener('click', () => {
+        content.forEach(c =>{
+            c.classList.remove('active')
+        } )
+        const t = document.querySelector(target.dataset.target)
+        t.classList.add('active')
+    } )
+} )
+
+
+
+//const li = document.querySelectorAll('.li')
+//const bloque = document.querySelectorAll('.bloque')
+//li.forEach( (cadaLi, i)=>{
+//    li[i].addEventListener('click',()=>{
+//        li.forEach( (cadaLi, i)=>{
+//            li[i].classList,remove('activo')
+//            bloque[i].classList,remove('activo')
+//        })
+//       a[i].classList.add('activo')
+//        bloque[i].classList.add('activo')
+//    })
+//
+//})
