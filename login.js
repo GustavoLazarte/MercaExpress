8449d2172a3e730f1882a5323f68b647c8f8790b
Richar Irasabal Belles <irasabal.richy@gmail.com>
2022-04-09 00:55:26 -0400

    se quito onliclick y funcion error

--- login.js
+++ login.js
@@ -80,16 +80,6 @@ function logout(e) {
 }
 
 
-function cerrar(){
-    firebaseConfig.auth().signOut()
-    .then(function(){
-        console.log('Salir');
-    })
-    .catch(function(error){
-        console.log(error);
-    })
-}
-
 /**e.preventDefault();
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
