b73c9733d282aa57bd8fe7915b29a6b37c9e8198
GustavoLazarte <gustucapo@gmail.com>
2022-04-10 23:16:04 -0400

    Se valido campos

--- usuario-super.js
+++ usuario-super.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 import { getFirestore, doc, getDoc, setDoc } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
-import { getStorage, ref, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js';
+import { getStorage, ref, getDownloadURL, uploadBytes } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js';
 //https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
     apiKey: "AIzaSyCzBQ9q4K8PzCOlPTqLBxra6I2V7HXuop8",
@@ -45,16 +45,19 @@ const form = document.getElementById('form-registro');
 
 form.addEventListener('submit', e => registrarVendedor(e));
 
-function registrarVendedor(e) {
+async function registrarVendedor(e) {
     e.preventDefault();
     if (contraseñaValida()) {
         const rol = 3;
-        const img = subirImagen();
+        const img = await subirImagen();
+        const email = document.getElementById('e-mail').value;
+        const password = document.getElementById('password').value;
+
         createUserWithEmailAndPassword(auth, email, password)
             .then(async (userCredential) => {
 
                 const docData = {
-                    email: document.getElementById('e-mail').value,
+                    email: email,
                     nombre: document.getElementById('nombre').value,
                     apellido: document.getElementById('apellido').value,
                     telefono: document.getElementById('telefono').value,
@@ -70,29 +73,40 @@ function registrarVendedor(e) {
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
-                // ..
+                console.log(errorMessage);
             });
     } else {
 
     }
+
+    /*console.log(document.getElementById('e-mail').value,)
+    console.log(document.getElementById('nombre').value,)
+    console.log(document.getElementById('apellido').value,)
+    console.log(document.getElementById('telefono').value,)
+    console.log(document.getElementById('dirección').value,)
+    console.log(document.getElementById('perfil').files[0].name)
+    console.log(subirImagen());*/
 }
 
-function subirImagen() {
+async function subirImagen() {
     const file = document.getElementById('perfil').files[0];
-    const mountainsRef = ref(storage, file.name);
-    let img;
-    let refer = "images/"+file.name;
-    
-    const mountainImagesRef = ref(storage, refer);
-    
-    const obtenerFoto = getDownloadURL(ref(storage, refer))
-        .then((url) => {
-            img = url;
-        });
 
+    const storage = getStorage();
+    const storageRef = ref(storage, 'images/' + file.name);
+
+    // 'file' comes from the Blob or File API
+    await uploadBytes(storageRef, file).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+    });
+
+    let img;
+    await getDownloadURL(ref(storage, 'images/' + file.name)).then((url) => {
+        img = url;
+    });
     return img;
+
 }
 
-function contraseñaValida(){
+function contraseñaValida() {
     return (document.getElementById('confirmar_contraseña').value) == (document.getElementById('password').value)
 }
