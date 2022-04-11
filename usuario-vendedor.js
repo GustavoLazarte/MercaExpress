8d0493745d5308293d610872ef41dc0706cf1eb0
GustavoLazarte <gustucapo@gmail.com>
2022-04-11 16:04:43 -0400

    Se cambio la logica del login

---
+++ usuario-vendedor.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { getFirestore, doc, getDoc, setDoc } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
+import { getStorage, ref, getDownloadURL, uploadBytes } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js';
+//https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyCzBQ9q4K8PzCOlPTqLBxra6I2V7HXuop8",
+    authDomain: "fir-mercaexpress.firebaseapp.com",
+    projectId: "fir-mercaexpress",
+    storageBucket: "fir-mercaexpress.appspot.com",
+    messagingSenderId: "801456117910",
+    appId: "1:801456117910:web:b88b8d9d4677852f070ab7"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+const storage = getStorage();
+
+window.onload = function () {
+    const auth = getAuth();
+    onAuthStateChanged(auth, async (user) => {
+        if (user) {
+            const uid = user.uid;
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+
+            const nom = docSnap.data().nombre;
+            const ap = docSnap.data().apellido;
+            const urlImg = docSnap.data().imgPerfil;
+            const img = document.getElementById('foto-Supervisor');
+            const em = document.getElementById('name');
+            img.setAttribute('src', urlImg);
+            em.innerHTML = "<span>" + nom + " " + ap + "</span>";
+
+        } else {
+
+            // User is signed out
+            // ...
+        }
+    });
+};
