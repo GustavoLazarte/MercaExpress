832fc4b4141e31349bff1b56770f89c3516f25ff
Richar Irasabal Belles <irasabal.richy@gmail.com>
2022-04-13 13:43:41 -0400

    se agrego la funcion, cuando estas en una pagina te muestra en cual pestaña estas resaltando de otro color

--- js/administrador.js
+++ js/administrador.js
@@ -9,30 +9,30 @@ const btn = document.querySelector('#menu-btn');
         });
 
 
-const targets = document.querySelectorAll('[data-target]')
-const content = document.querySelectorAll('[data-content]')
+//const targets = document.querySelectorAll('[data-target]')
+//const content = document.querySelectorAll('[data-content]')
+//
+//targets.forEach(target => {
+//    target.addEventListener('click', () => {
+//        content.forEach(c =>{
+//            c.classList.remove('active')
+//        } )
+//        const t = document.querySelector(target.dataset.target)
+//        t.classList.add('active')
+//    } )
+//} )
 
-targets.forEach(target => {
-    target.addEventListener('click', () => {
-        content.forEach(c =>{
-            c.classList.remove('active')
-        } )
-        const t = document.querySelector(target.dataset.target)
-        t.classList.add('active')
-    } )
-} )
 
-
-
-//const li = document.querySelectorAll('.li')
+//'use strict'
+//const li = document.querySelectorAll('.item')
 //const bloque = document.querySelectorAll('.bloque')
 //li.forEach( (cadaLi, i)=>{
 //    li[i].addEventListener('click',()=>{
 //        li.forEach( (cadaLi, i)=>{
-//            li[i].classList,remove('activo')
-//            bloque[i].classList,remove('activo')
+//            li[i].classList.remove('activo')
+//            bloque[i].classList.remove('activo')
 //        })
-//       a[i].classList.add('activo')
+//        li[i].classList.add('activo')
 //        bloque[i].classList.add('activo')
 //    })
 //
