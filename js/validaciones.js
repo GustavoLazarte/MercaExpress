839371a94685fcf9fdf0ca46ef299ce1f3c3fab8
Richar Irasabal Belles <irasabal.richy@gmail.com>
2022-04-14 23:27:41 -0400

    se centro los label e input del formulario de registro, tambien se cambio el contenido del formulario y label a % segun el tamaño de la pantalla

---
+++ js/validaciones.js
@@ -0,0 +1,6 @@
+
+//var input=
+//document.getElementById('nombre');
+//input.oninvalid =function(event){
+//    event.target.setCustomValidity('Se debe usar un minimo de 3 caracteres y solo son validos letras de a-z, A-Z.');
+//}
