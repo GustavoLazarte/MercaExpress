bdcb2dc1163bd19c48a1633eb673e63d49be54c2
GustavoLazarte <gustucapo@gmail.com>
2022-04-18 21:25:54 -0400

    Se arreglo el problema del telefono y la direccion

--- js/validaciones.js
+++ js/validaciones.js
@@ -4,18 +4,18 @@
 //    event.target.setCustomValidity('Se debe usar un minimo de 3 caracteres y solo son validos letras de a-z, A-Z.');
 //}
 function comprobarNombre() {
-  
+
   var mensaje = "";
-  
+
   // comprobar los posibles errores
   if (this.value == "") {
     mensaje = `El campo "nombre" no puede quedar vacio`;
   } else if (document.getElementById('nombre').value.length < 3) {
     caracter = document.getElementById('nombre').value.length;
     mensaje = "Debes ingresar 3 o mas carácteres, solo se permite letras a-Z y A-Z"
-    
+
   }
-  
+
   // mostrar/resetear mensaje (el mensaje se resetea poniendolo a "")
   this.setCustomValidity(mensaje);
 }
@@ -32,18 +32,18 @@ nombre.addEventListener("input", comprobarNombre);
 
 // Funcion para el campo apellido
 function comprobarApellido() {
-  
+
   var mensaje = "";
-  
+
   // comprobar los posibles errores
   if (this.value == "") {
     mensaje = `El campo "apellido" no puede quedar vacio`;
   } else if (document.getElementById('apellido').value.length < 3) {
     caracter = document.getElementById('apellido').value.length;
     mensaje = "Debes ingresar 3 o mas carácteres, solo se permite letras a-Z y A-Z"
-    
+
   }
-  
+
   // mostrar/resetear mensaje (el mensaje se resetea poniendolo a "")
   this.setCustomValidity(mensaje);
 }
@@ -61,14 +61,14 @@ apellido.addEventListener("input", comprobarApellido);
 
 // Funcion para el campo e-mail
 function comprobarEmail() {
-  
+
   var mensaje = "";
-  
+
   // comprobar los posibles errores
   if (this.value == "") {
     mensaje = `El campo "E-mail" no puede quedar vacio`;
   }
-  
+
   // mostrar/resetear mensaje (el mensaje se resetea poniendolo a "")
   this.setCustomValidity(mensaje);
 }
@@ -85,18 +85,31 @@ email.addEventListener("input", comprobarEmail);
 
 // Funcion para el campo telefono
 function comprobarTelefono() {
-  
+
   var mensaje = "";
-  
+  const regex = /^[0-9]*$/;
   // comprobar los posibles errores
   if (this.value == "") {
     mensaje = `El campo del "Teléfono" no puede quedar vacio`;
+  } else if (!regex.test(document.getElementById('telefono').value)) {
+    mensaje = "El campo no debe contener letras"
   } else if (document.getElementById('telefono').value.length < 7) {
     caracter = document.getElementById('telefono').value.length;
-    mensaje = "Debes ingresar una secuencia de numeros con 7 o mas digitos"
-    
+    mensaje = "Debes ingresar 7 u 8 dígitos"
+
+  } else {
+    if (document.getElementById('telefono').value.charAt(0) == '6' ||
+      document.getElementById('telefono').value.charAt(0) == '7') {
+      if (document.getElementById('telefono').value.length < 8) {
+        mensaje = "El Numero no es valido, debe tener 8 dígitos"
+      }
+    } else if(document.getElementById('telefono').value.charAt(0) == '4' && document.getElementById('telefono').value.length > 7) {
+      mensaje = "El Numero no es valido, debe tener 7 dígitos";
+    }else{
+      mensaje = "El Numero no es valido, debe iniciar con 4 ,7 o 8";
+    }
   }
-  
+
   // mostrar/resetear mensaje (el mensaje se resetea poniendolo a "")
   this.setCustomValidity(mensaje);
 }
@@ -112,14 +125,14 @@ telefono.addEventListener("input", comprobarTelefono);
 
 
 function comprobarDireccion() {
-  
+
   var mensaje = "";
-  
+
   // comprobar los posibles errores
   if (this.value == "") {
     mensaje = `El campo de "dirección" no puede quedar vacio`;
   }
-  
+
   // mostrar/resetear mensaje (el mensaje se resetea poniendolo a "")
   this.setCustomValidity(mensaje);
 }
@@ -135,16 +148,16 @@ direccion.addEventListener("input", comprobarDireccion);
 
 
 function comprobarPassword() {
-  
+
   var mensaje = "";
-  
+
   // comprobar los posibles errores
   if (this.value == "") {
     mensaje = `El campo de "password" no puede quedar vacio`;
   } else if (document.getElementById('password').value.length < 8) {
     mensaje = "Debes ingresar de 8 a 15 carácteres, debe contener por lo menos un número, una letra mayúscula y minúscula"
   }
-  
+
   // mostrar/resetear mensaje (el mensaje se resetea poniendolo a "")
   this.setCustomValidity(mensaje);
 }
@@ -160,18 +173,18 @@ password.addEventListener("input", comprobarPassword);
 
 
 function comprobarConfPassword() {
-  
+
   var mensaje = "";
-  
+
   // comprobar los posibles errores
   if (this.value == "") {
     mensaje = `El campo de "password" no puede quedar vacio`;
   } else if (document.getElementById('confirmar_contraseña').value.length < 8) {
     mensaje = "Debes ingresar de 8 a 15 carácteres, debe contener por lo menos un número, una letra mayúscula y minúscula"
-  } else if (document.getElementById('confirmar_contraseña').value != document.getElementById('password').value){
+  } else if (document.getElementById('confirmar_contraseña').value != document.getElementById('password').value) {
     mensaje = "Las contraseñas no coinciden"
   }
-  
+
   // mostrar/resetear mensaje (el mensaje se resetea poniendolo a "")
   this.setCustomValidity(mensaje);
 }
@@ -181,6 +194,8 @@ var confPassword = document.querySelector("#confirmar_contraseña");
 // cuando se cambie el valor del campo o sea incorrecto, mostrar/resetear mensaje
 confPassword.addEventListener("invalid", comprobarConfPassword);
 confPassword.addEventListener("input", comprobarConfPassword);
+
+
 
 
 
