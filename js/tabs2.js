c68fe6fab64b598d72d6017c469f09720c763702
Richar Irasabal Belles <irasabal.richy@gmail.com>
2022-04-27 20:30:41 -0400

    se modifico la vista para poder escoger una empresa para luego poder registrar su catalogo, tambien se implemento el formulario para poder registrar un catalogo de una empresa en usuario-suvervisor, usuari-administrador, y se agrego un jquery para poder ocultar el 1er formulario y dar visibilidad el 2do formulario esto dentro de catalogo

---
+++ js/tabs2.js
@@ -0,0 +1,26 @@
+//'use strict'
+//
+//const li = document.querySelectorAll('.hola')
+//const bloque = document.querySelectorAll('.bloque')
+//
+//li.forEach( function(cadaLi, i){
+//    li[i].addEventListener('click',function(){
+//        
+//        li.forEach( function(cadaLi, i){
+//            li[i].classList.remove('activa');
+//            //bloque[i].classList.remove('activo');
+//        });
+//
+//        //li[i].classList.add('activo');
+//        //bloque[i].classList.add('activo');
+//    
+//    });
+//});
+$("#contenedor_para_registrar_catalogo").hide();
+$(function(){
+    $("#ingresar").click(function(event){
+        $("#contenedor_ingresar_catalogo").hide();
+        $("#contenedor_para_registrar_catalogo").show();
+        event.preventDefault();
+    });
+});
